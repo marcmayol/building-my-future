6bfeee87f140994113200a197b8b3b0cba212712
Marc Mayol Orell <marcmayolorell@gmail.com>
2026-07-21 13:16:12 +0200

    Version 1.1 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.marc.gymplan100"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         vectorDrawables { useSupportLibrary = true }
     }
 
